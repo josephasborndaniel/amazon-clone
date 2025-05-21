02e19db99c04b14c918e13e963cf9d923155d05f
Joseph Asborn Daniel <josephasborndanielsmart07@gmail.com>
2025-05-21 23:59:39 +0530

    dc

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -5,7 +5,7 @@ import { useStateValue } from './StateProvider';
 import CheckoutProduct from './CheckoutProduct';
 
 function Checkout() {
- const [{basket},dispatch]= useStateValue();
+ const [{basket ,user},dispatch]= useStateValue();
  return (
  <div className=" w-full">
   <div className="flex justify-between">
@@ -19,6 +19,16 @@ function Checkout() {
       <Subtotal />
     </div>
   </div>
+<div class="max-w-4xl  px-4 sm:px-6 lg:px-8 py-8 bg-white rounded-2xl shadow-md mt-10">
+  <h3 class="text-lg sm:text-xl md:text-2xl font-semibold text-gray-800 mb-2">
+    Hello, <span class="text-blue-600 break-words">{user.email}</span>
+  </h3>
+  <h2 class="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900">
+    Your Shopping Basket
+  </h2>
+</div>
+
+
 
 <div className="w-4/10 border-gray-500">
     {basket.map((item, index) =>(<CheckoutProduct
